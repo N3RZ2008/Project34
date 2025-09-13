ec39a6086816296c54f87f54316d3dc1ec77a757
n3rz2008 <daniel.silva.roch2@gmail.com>
2025-09-13 12:49:18 -0300

    minor fixes

--- app/src/main/java/com/example/project34/MainActivity.java
+++ app/src/main/java/com/example/project34/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
